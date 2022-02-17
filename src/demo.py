c9dcebc08fd293cb67272de5a1cb8f5da4b42230
takuya-ki <taku8926@gmail.com>
2022-02-17 16:51:44 +0900

    modify

--- src/demo.py
+++ src/demo.py
@@ -26,7 +26,7 @@ def get_status(client):
 
 
 def get_fingertip_offset(client):
-    """Reads the current fingertip offset in 1/10 millimeters. 
+    """Reads the current fingertip offset in 1/10 millimeters.
        Please note that the value is a signed two's complement number.
     """
     result = client.read_holding_registers(address=258, count=1, unit=65)
@@ -57,7 +57,8 @@ def easy_control(client, command):
     """The control field is used to start and stop gripper motion.
        Only one option should be set at a time.
        Please note that the gripper will not start a new motion
-       before the one currently being executed is done (see busy flag in the Status field).
+       before the one currently being executed is done
+       (see busy flag in the Status field).
        The valid flags are:
 
        1: grip, Start the motion, with the preset target force and width.
@@ -65,13 +66,15 @@ def easy_control(client, command):
                 Please note that the gripper will ignore this command
                 if the busy flag is set in the status field.
        8: stop, Stop the current motion.
-       16: grip_w_offset, Same as grip, but width is calculated with the set fingertip offset.
+       16: grip_w_offset, Same as grip, but width is calculated
+                          with the set fingertip offset.
     """
     result = client.write_register(address=2, value=command, unit=65)
 
 
 def set_target_force(client, force_val):
-    """Writes the target force to be reached when gripping and holding a workpiece.
+    """Writes the target force to be reached
+       when gripping and holding a workpiece.
        It must be provided in 1/10th Newtons.
        The valid range is 0 to 400 for the RG2 and 0 to 1200 for the RG6.
     """
@@ -80,10 +83,12 @@ def set_target_force(client, force_val):
 
 
 def set_target_width(client, width_val):
-    """Writes the target width between the finger to be moved to and maintained.
+    """Writes the target width between
+       the finger to be moved to and maintained.
        It must be provided in 1/10th millimeters.
        The valid range is 0 to 1100 for the RG2 and 0 to 1600 for the RG6.
-       Please note that the target width should be provided corrected for any fingertip offset,
+       Please note that the target width should be provided
+       corrected for any fingertip offset,
        as it is measured between the insides of the aluminum fingers.
     """
     result = client.write_register(address=1, value=width_val, unit=65)
@@ -102,12 +107,12 @@ def run_demo():
     client.connect()
 
     if get_status(client) == 0:
-        print(get_fingertip_offset(client))
-        print(get_width_with_offset(client))
+        print("Current hand opening width: " +
+              str(get_width_with_offset(client)))
 
-        set_target_width(client, 1600)
+        set_target_width(client, 1600)  # fully opened
         time.sleep(1.0)
-        set_target_width(client, 0)
+        set_target_width(client, 0)  # fully closed
 
     client.close()
 
